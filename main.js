94283aaa980ce194efe9724ebb16939ca8c1d214
emmelykovalski <107261218+emmelykovalski@users.noreply.github.com>
2024-03-06 11:22:22 -0300

    06/03

---
+++ main.js
@@ -0,0 +1,27 @@
+constbotoes= document.querySelectorAll(".botao");
+for(let i= 0; i < botoes.length;+ +){
+botoes[i].onclick = function(){
+    for(let j=0;1<botoes.length;j+ +)
+{
+
+    
+}
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
